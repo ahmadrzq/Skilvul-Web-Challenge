8648d73312dbe9c14a91efca3b13ecb9ff9c8850
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-09-20 17:23:55 +0700

    Update script

--- script.js
+++ script.js
@@ -19,75 +19,74 @@ if (btnClose) {
 
 // Mengecek apakah inputan berupa angka
 function isNumber(number) {
-    if (isNaN(number)) {
-      return false;
+  if (isNaN(number)) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+// Mengecek apakah checkbox sudah tercentang
+function checkboxIsChecked() {
+  if (checkbox && checkbox.checked) {
+    console.log("checked");
+    return true;
+  } else {
+    console.log("Not checked");
+    return false;
+  }
+}
+const checkbox = document.getElementById("status");
+checkbox.addEventListener("click", function () {
+  checkboxIsChecked();
+});
+
+// Mengecek input kosong atau tidak
+function validateFormData(obj) {
+  if (obj != null && isNumber(obj.zipCode) && checkboxIsChecked()) {
+    return true;
+  }
+  return false;
+}
+
+// Event ketika mengklik tombol submit
+const form = document.querySelector("form");
+const warning = document.querySelector("#warning");
+
+function submit(e) {
+  const input = validateFormData(handleGetFormData());
+  if (input === false) {
+    e.preventDefault();
+    warning.innerHTML = "<p>Periksa form anda sekali lagi</p>";
+  } else {
+    warning.innerHTML = "";
+    clearInput();
+  }
+}
+
+form.addEventListener("submit", submit);
+
+// Menghapus inputan ketika submit sukses
+function clearInput() {
+  const inputs = document.querySelectorAll(
+    "#name, #city, #email, #zip-code, #status"
+  );
+  for (let input of inputs) {
+    if (input.type === "checkbox") {
+      input.checked = false;
+    } else if (input.type === "number") {
+      input.value = "";
     } else {
-      return true;
+      input.value = "";
     }
   }
+}
 
-  // Mengecek apakah checkbox sudah tercentang
-  function checkboxIsChecked() {
-    if (checkbox && checkbox.checked) {
-      console.log("checked");
-      return true;
-    } else {
-      console.log("Not checked");
-      return false;
-    }
-  }
-  const checkbox = document.getElementById("status");
-  checkbox.addEventListener("click", function () {
-    checkboxIsChecked();
-  });
-
-  // Mengecek input kosong atau tidak
-  function validateFormData(obj) {
-      if (obj != null && isNumber(obj.zipCode) && checkboxIsChecked()) {
-          return true
-      }
-      return false
-  }
-
-
-  // Event ketika mengklik tombol submit
-  const form = document.querySelector("form");
-  const warning = document.querySelector("#warning");
-
-  function submit(){
-    const input = validateFormData(handleGetFormData());
-    if (input === false) {
-      warning.innerHTML = "<p>Periksa form anda sekali lagi</p>";
-    } else {
-      warning.innerHTML = "";
-      clearInput();
-    }
-  }
-
-  form.addEventListener("submit", submit);
-
-
-  // Menghapus inputan ketika submit sukses
-  function clearInput() {
-    const inputs = document.querySelectorAll(
-      "#name, #city, #email, #zip-code, #status"
-    );
-    for (let input of inputs) {
-      if (input.type === "checkbox") {
-        input.checked = false;
-      } else if (input.type === "number") {
-        input.value = "";
-      } else {
-        input.value = "";
-      }
-    }
-  }
-
-  // Mengambil data dari inputan
-  const handleGetFormData = () => ({
-    name: document.getElementById("name").value,
-    email: document.getElementById("email").value,
-    city: document.getElementById("city").value,
-    zipCode: document.getElementById("zip-code").value,
-    status: document.getElementById("status").checked,
-  });
+// Mengambil data dari inputan
+const handleGetFormData = () => ({
+  name: document.getElementById("name").value,
+  email: document.getElementById("email").value,
+  city: document.getElementById("city").value,
+  zipCode: document.getElementById("zip-code").value,
+  status: document.getElementById("status").checked,
+});
